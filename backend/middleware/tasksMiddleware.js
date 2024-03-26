7c233172aa6d7f55fd65a294656ffdb60876fc8d
Tim Ngeno <timngeno424@gmail.com>
2024-03-26 16:37:34 +0300

    create db connection with mongoose

--- backend/middleware/tasksMiddleware.js
+++ backend/middleware/tasksMiddleware.js
@@ -1,10 +1,19 @@
 import dbClient from '../config/database.js';
 
 // Middleware function to initialize MongoDB client and get the tasks collection
-export default async function initializeMongoDB(req, res, next) {
+export default async function initializeMongoDB (req, res, next) {
   try {
-    const mongoClient = await dbClient.getMongoClient();
-    req.tasksCollection = await mongoClient.db().collection('tasks');
+    const mongoClient = await dbClient.connection;
+    // req.tasksCollection = await mongoClient.db().collection('tasks');
+
+    // Check if the connection is ready
+    if (mongoClient.readyState !== 1) {
+      console.error('MongoDB connection is not ready');
+      res.status(500).json({ error: 'MongoDB connection error' });
+      return;
+    }
+
+    req.tasksCollection = mongoClient.model('Task').collection;
     next();
   } catch (error) {
     console.error('Error initializing MongoDB', error);
