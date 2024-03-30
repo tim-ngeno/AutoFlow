18f82b16e000c8fe50fc138bef5bee881102eecb
Tim Ngeno <timngeno424@gmail.com>
2024-03-30 18:10:48 +0300

    rename route responsible for creating new tasks

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.use(initializeMongoDB);
 
 // Handle CRUD operations for all tasks
-router.post('/tasks', taskController.newTask);
+router.post('/tasks', taskController.createTask);
 router.get('/tasks', taskController.getAllTasks);
 router.get('/tasks/:taskId', taskController.getTaskById);
 router.put('/tasks/:taskId', taskController.updateTask);
