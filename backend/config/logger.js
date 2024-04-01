71441e69e4a0693419ca7b8bf2471bcdf3ad21ae
Tim Ngeno <timngeno424@gmail.com>
2024-04-01 11:11:35 +0300

    update logger to save error and informational logs in different files

--- backend/config/logger.js
+++ backend/config/logger.js
@@ -1,25 +1,36 @@
 import winston from 'winston';
 
-const { combine, cli, json, prettyPrint, timestamp } = winston.format;
+const { combine, cli, json, prettyPrint, simple, timestamp } = winston.format;
+
+const timezoned = () => {
+  return new Date().toLocaleString('en-US');
+};
 
 const logger = winston.createLogger({
   level: 'info',
   format: combine(
-    cli()
+    cli(),
+    timestamp({ format: timezoned })
   ),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File(
-      {
-        filename: 'autoflow.error.log',
-        level: 'error',
-        format: combine(
-	  json(),
-	  timestamp(),
-	  prettyPrint()
-        )
-      }
-    )
+    new winston.transports.File({
+      filename: 'autoflow.error.logs',
+      level: 'error',
+      format: combine(
+        json(),
+        timestamp({ format: timezoned }),
+        prettyPrint()
+      )
+    }),
+    new winston.transports.File({
+      filename: 'autoflow.logs',
+      format: combine(
+        json(),
+        timestamp({ format: timezoned }),
+        prettyPrint()
+      )
+    })
   ]
 });
 
