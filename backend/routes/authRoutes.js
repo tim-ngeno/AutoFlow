5873737d9a5672b1243bb06d26bd837bac56d144
Tim Ngeno <timngeno424@gmail.com>
2024-05-02 13:16:38 +0300

    Remove all .js files

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import authController from '../controllers/authController.js';
-import express from 'express';
-
-const router = express.Router();
-
-router.get('/', authController.getHome);
-router.get('/user', authController.getUser);
-
-export default router;
